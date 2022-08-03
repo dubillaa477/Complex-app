1796cd99488880ed59d0b7d52ebffb6f461a5fa3
dubilla <dubillaa477@ulacit.ed.cr>
2022-08-03 13:31:39 -0600

    adding mvc

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 const express = require('express')
 const app = express()
 
+const router = require('./router')
+
 //const port = process.env.PORT
 const port = 3000
 
 //Sets public folder as default 
 app.use(express.static('public'))
-    //Set up template engine
+
+//Set up template engine
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('home-guest')
-})
+//Seting up router actions
+app.use('/', router)
 
 app.listen(port)
